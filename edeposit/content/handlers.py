2421115840745a1d46df4c833ffb9aef6775f445
Jan Stavěl <stavel.jan@gmail.com>
2013-10-29 13:48:16 +0100

    fixed permissions for producent.

--- edeposit/content/handlers.py
+++ edeposit/content/handlers.py
@@ -3,7 +3,7 @@ from zope.component import queryUtility
 from zope.container.interfaces import IObjectAddedEvent, IObjectRemovedEvent,\
     IContainerModifiedEvent
 from zope.interface import Interface
-
+from plone import api
 from edeposit.user import MessageFactory as _
 
 def added(context,event):
@@ -13,7 +13,7 @@ def added(context,event):
                           title=_(u"Review of authors"),
                           query=[{'i': 'portal_type',
                                   'o': 'plone.app.querystring.operation.selection.is',
-                                  'v': 'edeposit.content.author'},
+                                  'v': ['edeposit.content.author',]},
                                  {'i': 'path', 
                                   'o': 'plone.app.querystring.operation.string.relativePath', 
                                   'v': '../'}
@@ -24,11 +24,50 @@ def added(context,event):
                           title=_(u"Review of ISBNs"),
                           query=[{'i': 'portal_type', 
                                   'o': 'plone.app.querystring.operation.selection.is', 
-                                  'v': 'edeposit.content.isbn'
+                                  'v': ['edeposit.content.isbn',]
                                   },
                                  {'i': 'path', 
                                   'o': 'plone.app.querystring.operation.string.relativePath', 
                                   'v': '../'}
                                  ],
                           )
+def addedEPublicationFolder(context, event):
+    def queryForStates(*args):
+        return [ {'i': 'portal_type',
+                  'o': 'plone.app.querystring.operation.selection.is',
+                  'v': ['edeposit.content.epublication']},
+                 {'i': 'review_state',
+                  'o': 'plone.app.querystring.operation.selection.is',
+                  'v': args},
+                 {'i': 'path', 
+                  'o': 'plone.app.querystring.operation.string.relativePath', 
+                  'v': '../'}
+                 ]
+    portal = api.portal.get()
+    collections = [ dict( contexts=[context],
+                          name = "ePublications-in-declarating",
+                          title=_(u"ePublications in declaring"),
+                          query= queryForStates('declaration')
+                          ),
+                    dict( contexts=[context],
+                          name = "ePublications-waiting-for-approving",
+                          title = _(u"ePublications waiting for preparing of acquisition"),
+                          query= queryForStates('waitingForApproving')
+                          ),
+                    dict( contexts=[context],
+                          name  = "ePublications-with-errors",
+                          title = _(u"ePublications with errors"),
+                          query = queryForStates('declarationWithError')
+                          )
+                    ]
+    return
+    for collection in collections:
+        for folder in collection['contexts']:
+            name = collection['name']
+            name in folder.keys() or \
+                folder.invokeFactory('Collection', name,
+                                     title=collection['title'],
+                                     query=collection['query'],
+                                     )
+            
     
