be8ca2dcd4fa0d9922f21772bbefbad0767aa5f3
Jan Stavěl <stavel.jan@gmail.com>
2014-04-23 10:25:58 +0200

    add starting script

---
+++ start-tmux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+/etc/init.d/varnish start
+
+cd /opt/edeposit
+./bin/supervisord
+
+tmux new-session -d -s edeposit
+ 
+tmux new-window     -t edeposit:1    -n 'supervisord'
+tmux split-window   -t edeposit:1    -h -h 'watch ./bin/supervisorctl status'
+tmux rotate-window  -t edeposit:1
+tmux new-window     -t edeposit:2    -n 'varnish histogram' 'varnishhist'
+tmux new-window     -t edeposit:3    -n 'edeposit' 'sudo su - edeposit'
+
+tmux ls | logger
+
+exit
+
