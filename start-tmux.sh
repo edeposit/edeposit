8494d5e8a34e86a6b90c15161156c39aa37d7241
Jan Stavěl <stavel.jan@gmail.com>
2014-05-09 09:45:29 +0200

    instance no. 4 will be for amqp communication only

--- start-tmux.sh
+++ start-tmux.sh
@@ -9,7 +9,7 @@ tmux split-window   -t edeposit:1    -h -h 'watch ./bin/supervisorctl status'
 tmux rotate-window  -t edeposit:1
 tmux new-window     -t edeposit:2    -n 'varnish histogram' 'varnishhist'
 tmux new-window     -t edeposit:3    -n 'edeposit' 'sudo su - edeposit'
-tmux new-window     -t edeposit:4    -n 'alephdaemon' '/usr/bin/python2.7 /usr/lib/python2.7/site-packages/edeposit/amqp/alephdaemon.py start'
+tmux new-window     -t edeposit:4    -n 'alephdaemon' '/usr/bin/python2.7 /usr/lib/python2.7/site-packages/edeposit/amqp/alephdaemon.py start --foreground'
 
 tmux ls | logger
 
