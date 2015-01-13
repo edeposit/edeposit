ad184015783fcb5163fac2c090d40009627048d1
Jan Stavěl <stavel.jan@gmail.com>
2015-01-13 15:31:43 +0100

    fixed tool to get data from aleph.

--- edeposit/content/utils.py
+++ edeposit/content/utils.py
@@ -2,18 +2,13 @@ from edeposit.amqp import aleph
 from functools import partial
 
 def loadFromAlephByISBN(isbn):
-    print "load from Aleph by ISBN"
-    isbn = '978-80-243-4116-3'
     result = aleph.reactToAMQPMessage(aleph.SearchRequest(aleph.ISBNQuery(isbn, base='nkc')),'UUID')
     return result.records
 
 
 def is_valid_isbn(isbn):
-    valid = aleph.isbn.is_valid_isbn(isbn)
-    return valid
+    return aleph.isbn.is_valid_isbn(isbn)
 
-
-def getISBNCount(isbn,base='nkc'):
-    appearedAtAleph = aleph.aleph.getISBNCount(isbn, base='nkc')
-    return
+def getISBNCount(isbn, base='nkc'):
+    return aleph.aleph.getISBNCount(isbn, base='nkc')
 
