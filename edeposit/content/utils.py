52b60f366fd97016e9e2bde8527ab628b1def061
Jan Stavěl <stavel.jan@gmail.com>
2015-03-31 17:33:47 +0200

    jstavel/edeposit#204 opravil jsem validaci isbn - uz pouziva novy balicek isbn-validator

--- edeposit/content/utils.py
+++ edeposit/content/utils.py
@@ -1,5 +1,6 @@
 from edeposit.amqp import aleph
 from functools import partial
+import isbn_validator
 
 def loadFromAlephByISBN(isbn):
     result = aleph.reactToAMQPMessage(aleph.SearchRequest(aleph.ISBNQuery(isbn, base='nkc')),'UUID')
@@ -7,7 +8,7 @@ def loadFromAlephByISBN(isbn):
 
 
 def is_valid_isbn(isbn):
-    return aleph.isbn.is_valid_isbn(isbn)
+    return isbn_validator.is_valid_isbn(isbn)
 
 def getISBNCount(isbn, base='nkc'):
     return aleph.aleph.getISBNCount(isbn, base='nkc')
