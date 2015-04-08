33793f88d97cdf7a78449e732dd442979936af0c
Jan Stavěl <stavel.jan@gmail.com>
2015-04-08 18:31:52 +0200

    jstavel/edeposit#245 pripravil jsem sablony pro nakladatel_vydavatel widget.

---
+++ edeposit/content/widgets/producent.py
@@ -0,0 +1,17 @@
+from zope.interface import implementer, implements, implementsOnly
+from zope.component import adapter
+from edeposit.content.originalfile import IVoucherFileField
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from z3c.form.interfaces import IFieldWidget, IFormLayer, IDataManager, NOVALUE
+from z3c.form.widget import FieldWidget
+from interfaces import IProducentWidget
+
+class VoucherFileWidget(NamedFileWidget):
+    implements(IVoucherFileWidget)
+    pass
+
+@implementer(IFieldWidget)
+@adapter(IVoucherFileField, IFormLayer)
+def VoucherFileFieldWidget(field, request):
+    return FieldWidget(field, VoucherFileWidget(request))
