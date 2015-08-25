596b1132a00d645d1678de271468acf7c1ed6f70
Jan Stavěl <stavel.jan@gmail.com>
2015-08-25 12:42:17 +0200

    doplnil jsem planovani dalsich ukolu

--- bin/send-emails-to-all-producents.sh
+++ bin/send-emails-to-all-producents.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-/opt/edeposit/bin/send-amqp-plone-task.py --file /opt/edeposit/docs/tests/resources/amqp/emails-to-all-producents-with-epublications-with-error.json
+/opt/edeposit/bin/send-amqp-plone-task.py --file /opt/edeposit/docs/tests/resources/amqp/emails-to-all-producents-with-epublications-in-declaration-with-error.json
+/opt/edeposit/bin/send-amqp-plone-task.py --file /opt/edeposit/docs/tests/resources/amqp/emails-to-all-producents-with-epublications-in-file-problem.json
+/opt/edeposit/bin/send-amqp-plone-task.py --file /opt/edeposit/docs/tests/resources/amqp/emails-to-all-producents-with-epublications-in-wrong-isbn-error.json
+/opt/edeposit/bin/send-amqp-plone-task.py --file /opt/edeposit/docs/tests/resources/amqp/emails-to-all-producents-with-epublications-in-only-czech-isbn-accepted.json
+/opt/edeposit/bin/send-amqp-plone-task.py --file /opt/edeposit/docs/tests/resources/amqp/emails-to-all-producents-with-epublications-in-isbn-already-exists-error.json
+/opt/edeposit/bin/send-amqp-plone-task.py --file /opt/edeposit/docs/tests/resources/amqp/emails-to-all-producents-with-epublications-in-datum-vydani-is-required-error.json
