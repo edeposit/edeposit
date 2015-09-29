cf2fe45324907c038f45de1ff0a43d6b6f7e8e7e
Jan Stavěl <stavel.jan@gmail.com>
2015-09-29 12:00:11 +0200

    fixed edeposit/edeposit#599 doplnil jsem funkcni skript, co posila update tick do aleph link export.

--- bin/send-amqp-aleph-link-exportd-tick.py
+++ bin/send-amqp-aleph-link-exportd-tick.py
@@ -3,13 +3,24 @@
 import pika
 import argparse
 import datetime
+from edeposit.amqp.aleph_link_export import StatusRequest
+from edeposit.amqp.serializers import serialize
+
+"""
+(project-shell "*shell*" "/usr/bin")
+(project-task send-amqp-aleph-link-exportd "*shell*" "send-amqp-aleph-link-exportd-tick.py")
+
+(local-set-key (kbd "C-c C-c") 'send-amqp-aleph-link-exportd)
+(local-set-key (kbd "C-c l") 'send-current-line-to-shell)
+"""
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Send message to RabbitMQ to update status at Aleph Link Exportd")
     pargs = parser.parse_args()
     conn = pika.BlockingConnection(pika.URLParameters("http://guest:guest@localhost:5672/aleph"))
     channel = conn.channel()
-    channel.basic_publish("update-links", "tick", "1", pika.BasicProperties(content_type="application/json",
-                                                                            headers=dict(UUID=str(datetime.datetime.now())),
-                                                                            delivery_mode=2))
-                                                                       
+    request=StatusRequest()
+    channel.basic_publish("update-links", "tick", serialize(request), 
+                          pika.BasicProperties(content_type="application/json",
+                                               headers=dict(UUID=str(datetime.datetime.now())),
+                                               delivery_mode=2))
