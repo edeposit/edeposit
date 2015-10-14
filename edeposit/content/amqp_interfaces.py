62e96caad2263433d09e14480ae619e50e112453
Jan Stavěl <stavel.jan@gmail.com>
2015-10-14 11:42:11 +0200

    cisteni

--- edeposit/content/amqp_interfaces.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from zope.interface import Interface, Attribute, implements, classImplements
-
-class IEmailSender(Interface):
-    def send():
-        pass
-
-
--- edeposit/content/widgets/interfaces.py
+++
@@ -1,7 +0,0 @@
-from plone.formwidget.namedfile.interfaces import INamedFileWidget
-
-class IVoucherFileWidget(INamedFileWidget):
-    pass
-
-class IOriginalFileSourceWidget(INamedFileWidget):
-    pass
