e2c5b799f3ca6419606ac1c9566d8613a789c7f6
Jan Stavěl <stavel.jan@gmail.com>
2015-11-03 13:23:58 +0100

    fixed edeposit/edeposit#686 doplnil jsem do subject informaci o tom, ze se jedna o testovaci server.

--- bin/send-amqp-plone-task.py
+++ bin/send-amqp-plone-task.py
@@ -2,14 +2,23 @@
 
 import pika
 import argparse
+import json
+import sys
+import os.path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+try:
+    from local import updateMsg
+except:
+    updateMsg = lambda msg: msg
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Send message to RabbitMQ to start some edeposit action")
     parser.add_argument('--file','-f', help="file with message to send", required=True)
     pargs = parser.parse_args()
-    msg = open(pargs.file,'rb').read()
+    msg = updateMsg(json.loads(open(pargs.file,'rb').read()))
     conn = pika.BlockingConnection(pika.URLParameters("http://guest:guest@localhost:5672/plone"))
     channel = conn.channel()
-    channel.basic_publish("task", "execute", msg, pika.BasicProperties(content_type="application/json",
+    channel.basic_publish("task", "execute", json.dumps(msg), pika.BasicProperties(content_type="application/json",
                                                                        delivery_mode=2))
                                                                        
